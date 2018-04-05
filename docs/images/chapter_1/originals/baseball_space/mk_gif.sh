2a4db6266fea4e1e83260fdd036c7f44d753b5ba
Doug Sweetser <sweetser@alum.mit.edu>
2018-04-05 18:23:50 -0400

    Deleted because the originals should not be here.

--- docs/images/chapter_1/originals/baseball_space/mk_gif.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-convert ../space-time_graphs_space.png -resize 9.85% small.png
-ls baseball* | perl -lane '$f = $F[0]; $n = $f; $n =~ s/baseball_space/space/; print(qq(convert +append $f small.png $n));'|zsh
-convert -delay 8 -loop 0 space_000* space.gif
-cp space.gif ../../baseball_animations
--- docs/images/chapter_1/originals/baseball_time/mk_gif.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-convert ../space-time_graphs_time.png -resize 9.85% small.png
-ls baseball* | perl -lane '$f = $F[0]; $n = $f; $n =~ s/baseball_time/time/; print(qq(convert +append $f small.png $n));'|zsh
-convert -delay 8 -loop 0 time_000* time.gif
-cp time.gif ../../baseball_animations
